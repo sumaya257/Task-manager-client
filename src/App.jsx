f8c86840d031181021fd3ee610e5877be2e97723
sumaya257 <sumayaece19@gmai.com>
2025-02-20 19:46:39 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      
+        
+      <h1 className="bg-black btn">Vite + React</h1>
+      <button className="btn">Default</button>
+     
+    </>
+  )
+}
+
+export default App
